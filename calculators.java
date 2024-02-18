1d7f5912e24539ca796e2ff67f9e65b315a53107
Akshay <01fe22bca025@kletech.ac.in>
2024-02-18 19:58:21 +0530

    Code for calculators

---
+++ calculators.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class calculators {
+
+    void calci() {
+        double num1 = 10, num2 = 20;
+
+        System.out.println("Num1=" + num1);
+        System.out.println("Num2=" + num2);
+
+        System.out.println("Addition=" + (num1 + num2));
+        System.out.println("Substraction=" + (num1 - num2));
+        System.out.println("Multiplication=" + (num1 * num2));
+        System.out.println("Division=" + (num1 / num2));
+    }
+}
+
+class arithmatic {
+    public static void main(String[] args) {
+        calculators z = new calculators();
+        z.calci();
+
+    }
+}
